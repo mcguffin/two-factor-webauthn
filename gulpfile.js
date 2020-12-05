eaa15080357d4ba903665108d7e2d9756c4e61b6
joernlund <joern@flyingletters.com>
2020-12-05 09:57:29 +0100

    fix gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ const autoprefixer	= require( 'gulp-autoprefixer' );
 const browserify	= require( 'browserify' );
 const babelify		= require( 'babelify' );
 const buffer		= require( 'vinyl-buffer' );
-const sass			= require( 'gulp-sass' );
+const sass			= require( '@selfisekai/gulp-sass' );
 const source		= require( 'vinyl-source-stream' );
 const sourcemaps	= require( 'gulp-sourcemaps' );
 const es			= require( 'event-stream' );
 const child_process	= require( 'child_process' );
+
+sass.compiler = require('node-sass');
 
 const package = require( './package.json' );
 
